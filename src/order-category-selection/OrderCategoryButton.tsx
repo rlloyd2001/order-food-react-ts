f06c4b9950ffe0774cc155dadd377dbed1ae00f9
Ron Lloyd <rlloyd2001@gmail.com>
2018-11-08 21:37:34 -0700

    adding click event to button and some code refactoring

--- src/order-category-selection/OrderCategoryButton.tsx
+++ src/order-category-selection/OrderCategoryButton.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
+import { OrderCategoryStore } from '../store-services/OrderCategoryStore';
 import './OrderCategoryButton.css';
 
 export default function OrderCategoryButton(props: Readonly<{ category: IOrderCategory }>) {
-  return <button type="button" className="btn btn-primary btn-lg category-button">
+  function selectCategory() {
+    OrderCategoryStore.selectedCategoryId.next(props.category.id);
+  }
+
+  return <button type="button" className="btn btn-primary btn-lg category-button" onClick={selectCategory}>
     {props.category.title}
   </button>;
 }
--- src/store-services/OrderCategoryStore.tsx
+++ src/store-services/OrderCategoryStore.tsx
@@ -1,27 +1,34 @@
-import { BehaviorSubject, merge } from 'rxjs';
+import { BehaviorSubject, merge, Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import { OrderCategoriesReader } from './OrderCategoriesReader';
 
 export class OrderCategoryStore {
-  public static categories: BehaviorSubject<IOrderCategory[]> = new BehaviorSubject<IOrderCategory[]>([]);
+  public static get categories(): Observable<IOrderCategory[]> {
+    return OrderCategoryStore.categoriesSubject.asObservable();
+  }
   public static selectedCategoryId: BehaviorSubject<string> = new BehaviorSubject<string>('');
-  public static selectedCategory: BehaviorSubject<IOrderCategory> = new BehaviorSubject<IOrderCategory>({} as any);
+  public static get selectedCategory(): Observable<IOrderCategory> {
+    return OrderCategoryStore.selectedCategorySubject.asObservable();
+  }
 
   public static initialize() {
     OrderCategoriesReader.find().subscribe((categories) => {
-      this.categories.next(categories);
+      this.categoriesSubject.next(categories);
     });
 
     merge(OrderCategoryStore.selectedCategoryId, OrderCategoryStore.categories).pipe(
-      filter(() => OrderCategoryStore.categories.getValue().length > 0),
+      filter(() => OrderCategoryStore.categoriesSubject.getValue().length > 0),
       filter(() => !!OrderCategoryStore.selectedCategoryId.getValue()),
       map(() => {
-       return OrderCategoryStore.categories.getValue()
+       return OrderCategoryStore.categoriesSubject.getValue()
         .find((value) => value.id === OrderCategoryStore.selectedCategoryId.getValue());
       }),
       filter(category => !!category)
     ).subscribe((category: IOrderCategory) => {
-      OrderCategoryStore.selectedCategory.next(category);
+      OrderCategoryStore.selectedCategorySubject.next(category);
     });
   }
+
+  private static selectedCategorySubject: BehaviorSubject<IOrderCategory> = new BehaviorSubject<IOrderCategory>({} as any);
+  private static categoriesSubject: BehaviorSubject<IOrderCategory[]> = new BehaviorSubject<IOrderCategory[]>([]);
 }
