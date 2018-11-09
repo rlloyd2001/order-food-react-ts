6d6ef209d63e02d8d43bf4b9a7e6554e041b6eec
Ron Lloyd <rlloyd2001@gmail.com>
2018-11-09 13:39:55 -0700

    adding back button to menu page

--- src/order-category-menu/OrderCategoryMenu.tsx
+++ src/order-category-menu/OrderCategoryMenu.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { OrderCategoryStore } from '../store-services/OrderCategoryStore';
 
 class OrderCategoryMenu extends React.Component {
   constructor(props: Readonly<{}>) {
@@ -7,8 +8,14 @@ class OrderCategoryMenu extends React.Component {
   }
 
   public render() {
+    function selectOtherCategory() {
+      OrderCategoryStore.selectedCategoryId.next('');
+    }
+
     return (
-      <div>TODO</div>
+      <div className="border" style={{width: '100%', padding: '14px', display: 'inline-block'}}>
+        <button style={{float: 'left'}} type="button" className="btn btn-secondary" onClick={selectOtherCategory}>Back</button>
+      </div>
     );
   }
 }
